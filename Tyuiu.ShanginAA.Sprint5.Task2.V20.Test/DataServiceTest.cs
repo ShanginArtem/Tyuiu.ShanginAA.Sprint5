2d8c02270e1a20f80cb70503bfdecc591ddf4239
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-12-26 01:22:17 +0500

    -

--- Tyuiu.ShanginAA.Sprint5.Task2.V20.Test/DataServiceTest.cs
+++ Tyuiu.ShanginAA.Sprint5.Task2.V20.Test/DataServiceTest.cs
@@ -9,6 +9,12 @@ namespace Tyuiu.ShanginAA.Sprint5.Task2.V20.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(fileExists, wait);
         }
     }
 }
