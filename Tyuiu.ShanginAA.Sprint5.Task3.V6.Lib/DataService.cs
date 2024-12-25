81199b2fa321e6ff413db41544cda643503077bc
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-12-26 01:27:37 +0500

    -

--- Tyuiu.ShanginAA.Sprint5.Task3.V6.Lib/DataService.cs
+++ Tyuiu.ShanginAA.Sprint5.Task3.V6.Lib/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.ShanginAA.Sprint5.Task3.V6.Lib
@@ -7,7 +8,15 @@ namespace Tyuiu.ShanginAA.Sprint5.Task3.V6.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
+            FileInfo fileInfo = new FileInfo(path);
+            if (fileInfo.Exists) fileInfo.Delete();
+            double y = Math.Round((double)x / (Math.Sqrt((double)x * (double)x + (double)x)), 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
         }
     }
 }
--- Tyuiu.ShanginAA.Sprint5.Task3.V6.Test/DataServiceTest.cs
+++ Tyuiu.ShanginAA.Sprint5.Task3.V6.Test/DataServiceTest.cs
@@ -9,6 +9,10 @@ namespace Tyuiu.ShanginAA.Sprint5.Task3.V6.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
         }
     }
 }
